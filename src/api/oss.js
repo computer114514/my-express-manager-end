fd06879b645fd1c943ebe3cd2add6b2720b5817f
computer114514 <3058094441@qq.com>
2026-07-27 09:34:46 +0800

    管理端调整

--- src/api/oss.js
+++ src/api/oss.js
@@ -3,7 +3,7 @@ import request from "../utils/request.js"
 export const getSign = () => {
     //投入个半小时时间也可以
     return request({
-        url: "/api/oss/sign",
+        url: "/api/user/oss/sign",
         method: "GET"
     });
 }
